779e224f823b3928fe99ebc3257e3eed55c6d960
Bharath Chandra <bharathnox@gmail.com>
2025-03-07 13:30:14 +0530

    avatar updated

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -8,7 +8,7 @@ export const DATA = {
   location: "Hyd, IN",
   locationLink: "https://www.google.com/maps/place/Hyderabad",
   description:
-    "Passionate undergraduate engineering student with a keen interest in web development and problem-solving. Always eager to learn and explore new technologies.",
+    "Passionate undergraduate engineering student with a keen interest in web development and problem solving. Always eager to learn and explore new technologies.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/mee.png",
